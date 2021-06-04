cfebe77a3b83c2a29b5d53bbfba1b848db9f8756
hrishi072 <parundekarrushi@gmail.com>
2021-06-04 17:53:36 +0530

    L

--- .zsh
+++ .zsh
@@ -1,5 +1,4 @@
-
-export ZSH="/home/hrishikesh/.oh-my-zsh"
+export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME="alanpeabody"
 ENABLE_CORRECTION="true"
 plugins=(git zsh-autosuggestions zsh-syntax-highlighting django python ubuntu sudo )
